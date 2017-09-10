865fa5e2ef754a0a01856590e0b2b3441f63f779
JapaneseTetris <jaimee.b.nye@gmail.com>
2017-09-10 04:33:08 -0400

    Code for the C Headers

---
+++ cryptoCHeaders.h
@@ -0,0 +1,7 @@
+#ifndef CRYPTO_C_HEADERS_H
+#define CRYPTO_C_HEADERS_H
+
+#include "C_Algorithms/c_BaseTen.h"
+#include "C_Algorithms/c_numberDefinitions.h"
+
+#endif
---
+++ cryptoCSource.cpp
@@ -0,0 +1,15 @@
+/*
+ * This file includes all of the .c files
+ * needed for this library.  It allows the
+ * CMake scripts for this project to be
+ * entirely C++ while still includeing raw
+ * C code.
+ *
+ */
+#ifndef CRYPTO_C_SOURCE_CPP
+#define CRYPTO_C_SOURCE_CPP
+
+#include "C_Algorithms/c_numberDefinitions.c"
+#include "C_Algorithms/c_BaseTen.c"
+
+#endif
